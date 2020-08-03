5c8212305cb8c0754324317eb87d5a8b0b1cdfa5
SydneyKao <69131885+SydneyKao@users.noreply.github.com>
2020-08-03 11:21:57 +0800

    Add files via upload

---
+++ Day1-1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:06:20 2020
+
+@author: AE401
+"""
+print('I am Eric \nHe is "cool"')
---
+++ Day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:58 2020
+
+@author: AE401
+"""
+
+Name = input("What's your name?\n")
+print('Hello',Name)
