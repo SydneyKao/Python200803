15af2d5f35a756cbbba4c97af4f3d7fa85226893
SydneyKao <69131885+SydneyKao@users.noreply.github.com>
2020-08-03 11:56:52 +0800

    Add files via upload

---
+++ Day1-3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:25:06 2020
+
+@author: AE401
+"""
+a=input('半徑:')
+b=float(3.1415926535)
+c=int(a)*float(b)*2
+d=int(a)*float(b)*int(a)
+print('周長是',c)
+print("面積是",d)
